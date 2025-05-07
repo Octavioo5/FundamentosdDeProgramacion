1c26614ea8ede72fd0ce49b583811e60a0e7cd06
Octavioo5 <124410741+Octavioo5@users.noreply.github.com>
2025-05-07 09:48:20 -0700

    Add files via upload

---
+++ Ejercicio4.py
@@ -0,0 +1,14 @@
+#Sergio Octavio Lopez Sainz 
+#24040636
+
+numA= int(input("\nIngresa un numero: "))
+numB= int(input("\nIngresa un numero: "))
+
+if numA == numB:
+    print("Los numeros son iguales")
+else:
+    if numA < numB:
+        print("El segundo valor es mayor")
+    else:
+        print("El primer valor es mayor")
+
